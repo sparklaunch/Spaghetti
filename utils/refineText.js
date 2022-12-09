dc52779532a0049b4562c4bd3c04deee6ad17ee9
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-09 10:13:18 +0900

    [Update] OCR 성능 개선.

--- utils/refineText.js
+++ utils/refineText.js
@@ -4,7 +4,15 @@ const refineText = chunks => {
     return chunk.includes("future");
   });
   const slicedChunks = chunks.slice(0, eFutureIndex - 1);
-  return slicedChunks[0];
+  console.log("Phase: ", slicedChunks);
+  if (slicedChunks.length === 1) {
+    const splitChunks = slicedChunks[0].split(" ");
+    return splitChunks;
+  } else if (slicedChunks.length === 2) {
+    return slicedChunks.join(" ").split(" ");
+  } else {
+    return slicedChunks;
+  }
 };
 
 export default refineText;
