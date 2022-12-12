2b71ca53337d530bbbc3ebba6f703be015be8d99
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-12 11:45:41 +0900

    [Add] 청크 바운스 애니메이션 추가.

--- components/Chunk.js
+++ components/Chunk.js
@@ -1,4 +1,4 @@
-import {Animated, Pressable, StyleSheet, Text} from "react-native";
+import {Animated, Easing, Pressable, StyleSheet, Text} from "react-native";
 import {useEffect, useRef} from "react";
 import Sound from "react-native-sound";
 import logError from "../utils/logError";
@@ -7,15 +7,30 @@ Sound.setCategory("Playback");
 
 const Chunk = ({chunk}) => {
   const springAnimation = useRef(new Animated.Value(0)).current;
+  const showSpringAnimation = Animated.spring(springAnimation, {
+    toValue: 1,
+    bounciness: 10,
+    speed: 1,
+    useNativeDriver: true
+  });
+  const bounceAnimation = Animated.timing(springAnimation, {
+    toValue: 1.5,
+    easing: Easing.inOut(Easing.ease),
+    useNativeDriver: true
+  });
+  const debounceAnimation = Animated.timing(springAnimation, {
+    toValue: 1,
+    easing: Easing.inOut(Easing.ease),
+    useNativeDriver: true
+  });
   const showSpring = () => {
-    Animated.spring(springAnimation, {
-      toValue: 1,
-      bounciness: 10,
-      speed: 1,
-      useNativeDriver: true
-    }).start();
+    showSpringAnimation.start();
+  };
+  const bounce = () => {
+    Animated.sequence([bounceAnimation, debounceAnimation]).start();
   };
   const onTapChunk = () => {
+    bounce();
     const sound = new Sound(`${chunk}.mp3`, Sound.MAIN_BUNDLE, error => {
       if (error) {
         logError("PLAY_SOUND_ERROR", error);
