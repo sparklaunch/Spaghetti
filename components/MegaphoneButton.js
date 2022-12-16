40f109ee6a45015cecec3e0285d36dbed354746b
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-16 12:09:48 +0900

    [Fix] 확성기 버튼 위치 재조정.

--- components/MegaphoneButton.js
+++ components/MegaphoneButton.js
@@ -1,16 +1,34 @@
 import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 import Constants from "../shared/Constants";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import DeviceVisibilityContext from "../contexts/DeviceVisibilityContext";
 
 const MegaphoneButton = ({onReplay}) => {
   const {isMegaphoneVisible} = useContext(DeviceVisibilityContext);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const onLayout = event => {
+    const {width, height} = event.nativeEvent.layout;
+    setWidth(width);
+    setHeight(height);
+  };
   return (
     <View
+      onLayout={onLayout}
       style={[
         styles.playButton,
         isMegaphoneVisible || {
           display: "none"
+        },
+        {
+          transform: [
+            {
+              translateX: -width / 2
+            },
+            {
+              translateY: height / 2
+            }
+          ]
         }
       ]}>
       <TouchableOpacity activeOpacity={0.5} onPress={onReplay}>
@@ -24,15 +42,7 @@ const styles = StyleSheet.create({
   playButton: {
     position: "absolute",
     bottom: `${Constants.TOP_OFFSET * 100}%`,
-    left: "50%",
-    transform: [
-      {
-        translateY: 50
-      },
-      {
-        translateX: -50
-      }
-    ]
+    left: "50%"
   }
 });
 
