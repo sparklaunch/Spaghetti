1b554090f8cffe04d1f0b9743c4d0946715bf220
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-16 12:12:04 +0900

    [Fix] 카메라 버튼 위치 재조정.

--- components/CameraButton.js
+++ components/CameraButton.js
@@ -1,16 +1,34 @@
 import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 import Constants from "../shared/Constants";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import DeviceVisibilityContext from "../contexts/DeviceVisibilityContext";
 
 const CameraButton = ({onTap}) => {
   const {isCameraVisible} = useContext(DeviceVisibilityContext);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const onLayout = event => {
+    const {width, height} = event.nativeEvent.layout;
+    setWidth(width);
+    setHeight(height);
+  };
   return (
     <View
+      onLayout={onLayout}
       style={[
         styles.tapButton,
         isCameraVisible || {
           display: "none"
+        },
+        {
+          transform: [
+            {
+              translateX: width / 2
+            },
+            {
+              translateY: -height / 2
+            }
+          ]
         }
       ]}>
       <TouchableOpacity activeOpacity={0.5} onPress={onTap}>
