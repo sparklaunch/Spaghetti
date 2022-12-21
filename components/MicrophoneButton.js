117f3b722185c6426a1cea3a22dc2dd7e46f6e2d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-21 13:54:11 +0900

    [Add] 마이크 비저블 컨텍스트 상태 추가.

--- components/MicrophoneButton.js
+++ components/MicrophoneButton.js
@@ -1,10 +1,12 @@
 import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import Constants from "../shared/Constants";
+import DeviceVisibilityContext from "../contexts/DeviceVisibilityContext";
 
 const MicrophoneButton = () => {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+  const {isMicrophoneVisible} = useContext(DeviceVisibilityContext);
   const onLayout = event => {
     const {width, height} = event.nativeEvent.layout;
     setWidth(width);
@@ -25,6 +27,9 @@ const MicrophoneButton = () => {
               translateY: height / 2
             }
           ]
+        },
+        isMicrophoneVisible || {
+          display: "none"
         }
       ]}>
       <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
--- contexts/DeviceVisibilityContext.js
+++ contexts/DeviceVisibilityContext.js
@@ -5,13 +5,16 @@ const DeviceVisibilityContext = createContext({});
 export const DeviceVisibilityContextProvider = ({children}) => {
   const [isCameraVisible, setIsCameraVisible] = useState(true);
   const [isMegaphoneVisible, setIsMegaphoneVisible] = useState(false);
+  const [isMicrophoneVisible, setIsMicrophoneVisible] = useState(false);
   return (
     <DeviceVisibilityContext.Provider
       value={{
         isCameraVisible,
         setIsCameraVisible,
         isMegaphoneVisible,
-        setIsMegaphoneVisible
+        setIsMegaphoneVisible,
+        isMicrophoneVisible,
+        setIsMicrophoneVisible
       }}>
       {children}
     </DeviceVisibilityContext.Provider>
