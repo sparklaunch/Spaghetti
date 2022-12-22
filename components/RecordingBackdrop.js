35ce0c0a0cd55506c57db64a095ddd3c9b3e7a69
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-22 18:22:59 +0900

    [Add] 스픽 나우 추가.

--- components/RecordingBackdrop.js
+++ components/RecordingBackdrop.js
@@ -1,5 +1,10 @@
-import {Image, StyleSheet, TouchableOpacity} from "react-native";
-import Animated, {FadeIn, FadeOut} from "react-native-reanimated";
+import {StyleSheet, TouchableOpacity} from "react-native";
+import Animated, {
+  BounceIn,
+  BounceOut,
+  FadeIn,
+  FadeOut
+} from "react-native-reanimated";
 import Constants from "../shared/Constants";
 import useStopRecording from "../hooks/useStopRecording";
 
@@ -14,7 +19,15 @@ const RecordingBackdrop = () => {
         activeOpacity={0.5}
         onPress={onPress}
         style={styles.touchableOpacity}>
-        <Image
+        <Animated.Image
+          source={require("../assets/images/speak_now.png")}
+          entering={BounceIn}
+          exiting={BounceOut}
+          style={styles.speakNow}
+        />
+        <Animated.Image
+          entering={BounceIn}
+          exiting={BounceOut}
           source={require("../assets/images/microphone.png")}
           style={styles.microphone}
         />
@@ -30,7 +43,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.75)"
+    backgroundColor: "rgba(255, 255, 255, 0.8)"
   },
   touchableOpacity: {
     flex: 1
@@ -40,12 +53,14 @@ const styles = StyleSheet.create({
     width: 76,
     height: 76,
     bottom: `${Constants.TOP_OFFSET * 100}%`,
-    left: "50%",
-    transform: [
-      {
-        translateX: -38
-      }
-    ]
+    left: "45%"
+  },
+  speakNow: {
+    position: "absolute",
+    width: 561,
+    height: 33,
+    top: `${Constants.TOP_OFFSET * 100}%`,
+    left: "15%"
   }
 });
 
