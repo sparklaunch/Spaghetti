ae1bb945a9072d3766b0099dd56fd73ab9afa473
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-22 16:16:51 +0900

    [Add] 마이크 애니메이션 mp4 파일 추가.

--- components/RecordingBackdrop.js
+++ components/RecordingBackdrop.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.5)"
+    backgroundColor: "rgba(255, 255, 255, 0.75)"
   }
 });
 
