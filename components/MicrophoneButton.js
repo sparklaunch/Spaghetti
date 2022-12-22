2d747fe5c7ce5d893c04dc73369638635e038976
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-22 09:28:54 +0900

    [Add] Axios를 통한 API 요청 기능 추가.

--- components/MicrophoneButton.js
+++ components/MicrophoneButton.js
@@ -4,6 +4,7 @@ import DeviceVisibilityContext from "../contexts/DeviceVisibilityContext";
 import SoundRecorder from "react-native-sound-recorder";
 import useErrorHandler from "../hooks/useErrorHandler";
 import RNFS from "react-native-fs";
+import axios from "axios";
 
 const MicrophoneButton = () => {
   const errorHandler = useErrorHandler();
@@ -32,8 +33,26 @@ const MicrophoneButton = () => {
           const {path} = result;
           console.log("Result saved in " + path);
           RNFS.readFile(path, "base64")
-            .then(response => {
-              console.log(response);
+            .then(audio => {
+              axios
+                .post(
+                  "https://api.elasolution.com/pron_v2/phoneme",
+                  {
+                    audio,
+                    text: "mug"
+                  },
+                  {
+                    headers: {
+                      "X-API-KEY": "feafc8ee6fa249d496369ac40b256b95"
+                    }
+                  }
+                )
+                .then(response => {
+                  console.log(response);
+                })
+                .catch(error => {
+                  console.log(error);
+                });
             })
             .catch(error => {
               errorHandler("READ_AUDIO_ERROR", error);
