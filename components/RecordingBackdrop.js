875aab090f192cba9c2fecedd152027432c447c2
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-22 17:48:25 +0900

    [Add] 마이크 이미지 추가.

--- components/RecordingBackdrop.js
+++ components/RecordingBackdrop.js
@@ -1,12 +1,16 @@
-import {StyleSheet} from "react-native";
+import {StyleSheet, Image} from "react-native";
 import Animated, {FadeIn, FadeOut} from "react-native-reanimated";
+import Video from "react-native-video";
+import Constants from "../shared/Constants";
 
 const RecordingBackdrop = () => {
   return (
-    <Animated.View
-      style={styles.block}
-      entering={FadeIn}
-      exiting={FadeOut}></Animated.View>
+    <Animated.View style={styles.block} entering={FadeIn} exiting={FadeOut}>
+      <Image
+        source={require("../assets/images/microphone.png")}
+        style={styles.microphone}
+      />
+    </Animated.View>
   );
 };
 
@@ -18,6 +22,18 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     backgroundColor: "rgba(255, 255, 255, 0.75)"
+  },
+  microphone: {
+    position: "absolute",
+    width: 76,
+    height: 76,
+    bottom: `${Constants.TOP_OFFSET * 100}%`,
+    left: "50%",
+    transform: [
+      {
+        translateX: -38
+      }
+    ]
   }
 });
 
