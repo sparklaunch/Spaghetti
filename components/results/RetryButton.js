2a13110cee19d9658eb2b09ddffecd2cf7f2f040
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-23 15:03:41 +0900

    [Add] 다시 시도 기능 추가.

--- components/results/RetryButton.js
+++ components/results/RetryButton.js
@@ -1,7 +1,17 @@
 import {Image, StyleSheet, TouchableOpacity} from "react-native";
+import {useContext} from "react";
+import ResultsStatusContext from "../../contexts/ResultsStatusContext";
+import RecordingStatusContext from "../../contexts/RecordingStatusContext";
+import useStartRecording from "../../hooks/useStartRecording";
 
 const RetryButton = () => {
-  const retry = () => {};
+  const {setResultsScreenShown} = useContext(ResultsStatusContext);
+  const {setIsRecording} = useContext(RecordingStatusContext);
+  const startRecording = useStartRecording();
+  const retry = () => {
+    setResultsScreenShown(false);
+    startRecording();
+  };
   return (
     <TouchableOpacity
       activeOpacity={0.5}
