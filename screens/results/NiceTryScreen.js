646341806780c2d1d83d19600c1e09939c6ea578
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-23 11:45:14 +0900

    [Add] NiceTryScreen.js 추가.

---
+++ screens/results/NiceTryScreen.js
@@ -0,0 +1,28 @@
+import Animated, {FadeIn, FadeOut} from "react-native-reanimated";
+import GoodBackground from "../../components/results/good/GoodBackground";
+import GoodText from "../../components/results/good/GoodText";
+import GoBackButton from "../../components/results/GoBackButton";
+import RetryButton from "../../components/results/RetryButton";
+import ChunkBoxContainer from "../../components/results/ChunkBoxContainer";
+import {StyleSheet} from "react-native";
+
+const NiceTryScreen = () => {
+  return (
+    <Animated.View style={styles.container} entering={FadeIn} exiting={FadeOut}>
+      <GoodBackground />
+      <GoodText />
+      <GoBackButton />
+      <RetryButton />
+      <ChunkBoxContainer />
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#80CDEF"
+  }
+});
+
+export default NiceTryScreen;
