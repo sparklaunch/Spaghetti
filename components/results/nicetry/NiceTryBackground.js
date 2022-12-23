afa2ddbfa6f456ac43db9f66cd26db42e98fb496
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-23 11:50:05 +0900

    [Add] 봉봉(?) 이미지 추가.

--- components/results/nicetry/NiceTryBackground.js
+++ components/results/nicetry/NiceTryBackground.js
@@ -8,8 +8,8 @@ const NiceTryBackground = () => {
         style={styles.alligator}
       />
       <Image
-        source={require("../../../assets/images/fox_train.png")}
-        style={styles.foxTrain}
+        source={require("../../../assets/images/bonbon.png")}
+        style={styles.bonbon}
       />
     </View>
   );
@@ -28,10 +28,10 @@ const styles = StyleSheet.create({
     bottom: 50,
     left: 30
   },
-  foxTrain: {
+  bonbon: {
     position: "absolute",
-    bottom: 10,
-    right: 10
+    bottom: 30,
+    right: 30
   }
 });
 
