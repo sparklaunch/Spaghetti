0e67d3980a34fb55f06f003b43306fc65d3961f8
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-23 15:35:46 +0900

    [Refactor] Good, mediocre, bad 기준 점수 분리.

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -3,13 +3,14 @@ import ResultsContext from "../contexts/ResultsContext";
 import ExcellentScreen from "./results/ExcellentScreen";
 import GoodScreen from "./results/GoodScreen";
 import NiceTryScreen from "./results/NiceTryScreen";
+import Constants from "../shared/Constants";
 
 const ResultsScreen = () => {
   const {results} = useContext(ResultsContext);
   const sentenceScore = results.sentence_score;
-  if (sentenceScore >= 80) {
+  if (sentenceScore >= Constants.GOOD_THRESHOLD) {
     return <ExcellentScreen />;
-  } else if (sentenceScore >= 60) {
+  } else if (sentenceScore >= Constants.MEDIOCRE_THRESHOLD) {
     return <GoodScreen />;
   } else {
     return <NiceTryScreen />;
