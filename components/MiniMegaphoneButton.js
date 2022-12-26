0c7e6a3c095b9a2845a313ba907e6f132a6160ba
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-12-26 15:36:35 +0900

    [Add] 디바이더 확성기 추가.

--- components/MiniMegaphoneButton.js
+++ components/MiniMegaphoneButton.js
@@ -1,12 +1,37 @@
-import {Image, TouchableOpacity} from "react-native";
+import {Image, StyleSheet, TouchableOpacity} from "react-native";
 
-const MiniMegaphoneButton = () => {
+const MiniMegaphoneButton = ({order}) => {
   const onPress = () => {};
   return (
-    <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
-      <Image source={require("../assets/images/megaphone.png")} />
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={onPress}
+      style={[
+        styles.container,
+        {
+          left: order === "first" ? "34.5%" : "60.5%"
+        }
+      ]}>
+      <Image
+        source={require("../assets/images/megaphone.png")}
+        style={styles.button}
+      />
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    zIndex: 10,
+    top: "45%",
+    width: 100,
+    height: 100
+  },
+  button: {
+    width: 40,
+    height: 40
+  }
+});
 
 export default MiniMegaphoneButton;
--- components/MiniMegaphoneButtons.js
+++ components/MiniMegaphoneButtons.js
@@ -1,17 +1,21 @@
 import {StyleSheet, View} from "react-native";
 import MiniMegaphoneButton from "./MiniMegaphoneButton";
+import {useContext} from "react";
+import ChunksContext from "../contexts/ChunksContext";
 
 const MiniMegaphoneButtons = () => {
+  const {chunks} = useContext(ChunksContext);
   return (
     <View style={styles.container}>
-      <MiniMegaphoneButton />
-      <MiniMegaphoneButton />
+      <MiniMegaphoneButton order={"first"} />
+      <MiniMegaphoneButton order={"last"} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     position: "absolute",
     top: 0,
     left: 0,
